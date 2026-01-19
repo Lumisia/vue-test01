9c2e9dffa3b67cd8f558f3cdd2b27433d4b09bc2
kjh <rlawngud1999@gmail.com>
2026-01-19 10:41:23 +0900

    Feat: 유저 정보 반영

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -3,17 +3,26 @@ import { ref } from 'vue'
 
 export const useAuthStore = defineStore('auth', () => {
   const isLogin = ref(false)
-
+  const user = ref(null)
   const login = (userInfo) => {
     isLogin.value = true
+    const userData = typeof userInfo === 'string' ? JSON.parse(userInfo) : userInfo
+    user.value = userData
     localStorage.setItem('USERINFO', userInfo)
   }
 
   const checkLogin = () => {
-    if (localStorage.getItem('USERINFO')) {
+    const savedInfo = localStorage.getItem('USERINFO')
+    if (savedInfo) {
       isLogin.value = true
+      user.value = JSON.parse(savedInfo)
     }
     return isLogin.value
   }
-  return { isLogin, login, checkLogin }
+  const logout = () => {
+    isLogin.value = false
+    user.value = null
+    localStorage.removeItem('USERINFO')
+  }
+  return { isLogin, user, login, checkLogin, logout }
 })
